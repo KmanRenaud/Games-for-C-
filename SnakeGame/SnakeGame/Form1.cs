7be1668dc5b426a95131b7a66dea842341302078
Kael Renaud <Kmans2272@gmail.com>
2022-10-03 09:03:42 -0500

    HEY

---
+++ SnakeGame/SnakeGame/Form1.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Circle> Snake = new List<Circle>();
+        private Circle food = new Circle();
+
+        
+
+        int maxWidth;
+        int maxHeight;
+
+        int score;
+        int highscore;
+
+
+        Random rand = new Random();
+
+        bool goLeft, goRight, goUp, goDown;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            new Settings();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left && Settings.directions != "right")
+            {
+                goLeft = true;
+            }
+            if (e.KeyCode == Keys.Right && Settings.directions != "left")
+            {
+                goRight = true;
+            }
+            if (e.KeyCode == Keys.Up && Settings.directions != "down")
+            {
+                goUp = true;
+            }
+            if (e.KeyCode == Keys.Down && Settings.directions != "up")
+            {
+                goDown = true;
+            }
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                goLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                goRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                goUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                goDown = false;
+            }
+
+
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+            RestartGame();
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+            if (goLeft)
+            {
+                Settings.directions = "left";
+            }
+
+            if (goRight)
+            {
+                Settings.directions = "right";
+            }
+
+            if (goUp)
+            {
+                Settings.directions = "up";
+            }
+
+            if (goDown)
+            {
+                Settings.directions = "down";
+            }
+
+
+
+
+            for (int i = Snake.Count - 1; i >= 0; i--)
+            {
+                if (i == 0)
+                {
+                    switch (Settings.directions)
+                    {
+                        case "left":
+                            Snake[i].X--;
+                            break;
+                        case "right":
+                            Snake[i].X++;
+                            break;
+                        case "up":
+                            Snake[i].Y--;
+                            break;
+                        case "down":
+                            Snake[i].Y++;
+                            break;
+                        default:
+                            break;
+                    }
+
+
+                    if (Snake[i].X < 0)
+                    {
+                        Snake[i].X = maxWidth;
+                    }
+                    if (Snake[i].X > maxWidth)
+                    {
+                        Snake[i].X = 0;
+                    }
+                    if (Snake[i].Y < 0)
+                    {
+                        Snake[i].Y = maxHeight;
+                    }
+                    if (Snake[i].Y > maxHeight)
+                    {
+                        Snake[i].Y = 0;
+                    }
+
+                    if (Snake[i].X == food.X && Snake[i].Y == food.Y)
+                    {
+                        EatFood();
+                    }
+                    for (int j = 1; j < Snake.Count; j++)
+                    {
+                        if (Snake[i].X == Snake[j].X && Snake[i].Y == Snake[j].Y)
+                        {
+                            GameOver();
+                        }
+                    }
+
+
+                }
+                else
+                {
+                    Snake[i].X = Snake[i - 1].X;
+                    Snake[i].Y = Snake[i - 1].Y;
+                }
+
+            }
+
+            picCanvas.Invalidate();
+
+        }
+
+        private void UpdatePictureBoxGraphics(object sender, PaintEventArgs e)
+        {
+
+            Graphics canvas = e.Graphics;
+
+            Brush snakeColor;
+
+            for (int i = 0; i < Snake.Count; i++)
+            {
+                if (i == 0)
+                {
+                    snakeColor = Brushes.DarkBlue;
+                }
+                else
+                {
+                    snakeColor = Brushes.LightSkyBlue;
+                }
+
+                canvas.FillEllipse(snakeColor, new Rectangle(
+                    Snake[i].X * Settings.Width, 
+                    Snake[i].Y * Settings.Height,
+                    Settings.Width, Settings.Height
+                    ));
+            }
+
+            canvas.FillEllipse(Brushes.DarkRed, new Rectangle(
+            food.X * Settings.Width,
+            food.Y * Settings.Height,
+            Settings.Width, Settings.Height
+            ));
+
+
+        }
+
+        private void TakeSnapShot(object sender, EventArgs e)
+        {
+
+            Label caption = new Label();
+            caption.Text = "I just scored: " + score + " and my highscore is: " + highscore + "!!!";
+            caption.Font = new Font("Merriweather", 12, FontStyle.Bold);
+            caption.ForeColor = Color.DarkViolet;
+            caption.AutoSize = false;
+            caption.Width = picCanvas.Width;
+            caption.Height = 30;
+            caption.TextAlign = ContentAlignment.MiddleCenter;
+            picCanvas.Controls.Add(caption);
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = "Snake Game Snapshot";
+            dialog.DefaultExt = "jpg";
+            dialog.Filter = "JPG Image File | *.jpg";
+            dialog.ValidateNames = true;
+
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                int width = Convert.ToInt32(picCanvas.Width);
+                int height = Convert.ToInt32(picCanvas.Height);
+                Bitmap bmp = new Bitmap(width, height);
+                picCanvas.DrawToBitmap(bmp, new Rectangle(0, 0, width, height));
+                bmp.Save(dialog.FileName, ImageFormat.Jpeg);
+                picCanvas.Controls.Remove(caption);
+            }
+
+        }
+
+        private void RestartGame()
+        {
+
+
+            maxWidth = picCanvas.Width / Settings.Width - 1;
+            maxHeight = picCanvas.Height / Settings.Height - 1;
+
+            Snake.Clear();
+
+            startButton.Enabled = false;
+            snapButton.Enabled = false;
+            score = 0;
+            txtScore.Text = "Score: " + score;
+
+            Circle head = new Circle() { X = 10, Y = 5 };
+            Snake.Add(head);
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Circle body = new Circle();
+                Snake.Add(body);
+            }
+
+            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight)};
+
+            gameTimer.Start();
+
+
+
+
+
+
+        }
+
+        private void EatFood()
+        {
+
+            score += 1;
+            gameTimer.Interval -= 2;
+            if (gameTimer.Interval < 32)
+            {
+                gameTimer.Interval += 2;
+            }
+            txtScore.Text = "Score: " + score;
+
+            Circle body = new Circle
+            {
+                X = Snake[Snake.Count - 1].X,
+                Y = Snake[Snake.Count - 1].Y
+            };
+
+            Snake.Add(body);
+
+            food = new Circle { X = rand.Next(2, maxWidth), Y = rand.Next(2, maxHeight) };
+
+
+
+
+        }
+
+        private void GameOver()
+        {
+            gameTimer.Stop();
+            gameTimer.Interval = 50;
+            startButton.Enabled =  true;
+            snapButton.Enabled = true;
+
+            if (highscore < score)
+            {
+                highscore = score;
+
+                txtHighScore.Text = "High Score: " + Environment.NewLine + highscore;
+                txtHighScore.ForeColor = Color.Green;
+                txtHighScore.TextAlign = ContentAlignment.MiddleCenter;
+            }
+        }
+    }
+}
---
+++ SnakeGame/SnakeGame/Settings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeGame
+{
+    internal class Settings
+    {
+
+        public static int Width { get; set; }
+            
+        public static int Height { get; set; }
+        public static int Speed { get; set; }
+
+        public static string directions;
+
+        public Settings()
+        {
+            Speed = 0;
+            Width = 16;
+            Height = 16;
+            directions = "left";
+        }
+
+
+
+    }
+}
